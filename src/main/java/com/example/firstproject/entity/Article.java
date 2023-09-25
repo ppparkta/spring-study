70273fd6d9f20432d174ce29c2be8ac1b04b0d0f
ppparkta <pppppark2@gmail.com>
2023-09-25 17:03:15 +0900

    feat: JPA loggin setting

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -1,9 +1,6 @@
 package com.example.firstproject.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @ToString
@@ -14,7 +11,7 @@ import lombok.*;
 @Setter
 public class Article {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column
     private String title;
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO article(id, title, content) VALUES(1, 'test1', 'content1');
-INSERT INTO article(id, title, content) VALUES(2, 'test2', 'content2');
-INSERT INTO article(id, title, content) VALUES(3, 'test3', 'content3');
-INSERT INTO member(id, email, pwd) VALUES (1, 'qwer@naver.com', 'qwer1234');
+INSERT INTO article(title, content) VALUES('test1', 'content1');
+INSERT INTO article(title, content) VALUES('test2', 'content2');
+INSERT INTO article(title, content) VALUES('test3', 'content3');
+INSERT INTO member(email, pwd) VALUES ('qwer@naver.com', 'qwer1234');
